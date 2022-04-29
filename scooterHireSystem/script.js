0b5787bcb5077051de7880ff866945d546fd2d04
Pomegranule <elleg1@gmx.com>
2022-04-29 12:26:14 +0100

    Group with Ali and Ahmad
    We getting that bread.
    
    Also, we installed node and nano id. We have started with the basic classes ( user, customer, scooter).

---
+++ scooterHireSystem/script.js
@@ -0,0 +1,53 @@
+"use strict";
+const nanoid = require("nanoid")
+
+class User{
+    constructor(userID){
+        
+    }
+}
+
+
+
+class Customer extends User{
+    constructor(BankBalance){
+
+        super()
+        this.BankBalance=BankBalance
+    }
+    rentScooter(){
+
+        
+    }
+
+    returnScooter(){
+
+        
+    }
+    
+    returnBrokenScooter(){
+
+
+        
+    }
+
+}
+
+class maintW extends User{
+    
+}
+
+class Scooter {
+    // static scooters =  
+    maxRange = 32;
+
+    constructor(scooter){
+        this.scooter = scooter;
+    }
+}
+
+
+class ChargingDock{
+
+    
+}
