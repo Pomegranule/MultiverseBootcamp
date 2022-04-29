4261ed2304b79ac31dd53ddc3b22799b147bf78f
Pomegranule <elleg1@gmx.com>
2022-04-29 13:54:15 +0100

    Commit 2: The electric Boogaloo
    We have added another file that contains a file full of helpful things from Simon. We have also created a new method for charging without the need for so many arrays. Ali has started on this new methodology.

---
+++ scooterHireSystem/helpfulThingsFromSimon.js
@@ -0,0 +1,91 @@
+const phones =  [
+    {
+        id: 1,
+        model: "nokia",
+        charge: 0.12,
+    },
+    {
+        id: 2,
+        model: "apple",
+        charge: 0.78,
+    },
+    {
+        id: 3,
+        model: "nokia",
+        charge: 0.42,
+    },
+];
+
+const filteredPhones = phones.filter(p => p.model === "nokia");
+console.log(filteredPhones);
+
+// This is a charging model that was proposed by Simon, using phones.
+
+
+
+// This is another charging models.
+class ChargingDock {
+    scooters = [];
+    get availableScooters () {
+        return this.scooters.filter(s=> s.charged === true);
+    };
+};
+
+const scooter = {
+    charged: true,
+    broken: false,
+    startCharging () {
+        setTimeout(this.charged = true, 10000);//Keeps the scooter in the dock for 10 seconds.
+    },
+};
+
+const user = {
+
+    currentScooter: scooter,
+
+    returnScooter(dock) {
+        if (this.currentScooter) {
+            dock.scooters.push(this.currentScooter); //Putting the scooter into the dock.
+            this.currentScooter.startcharging(); // It starts charging.
+            this.currentScooter = null //This needs to be last so that it only removes the scooter from the user once it has been charged.
+        };
+    },
+};
+
+const nanoid =  require("nanoid")
+
+class Scooter {
+    static allScooter = []
+
+    static get scooterCount(){
+        return Scooter.allScooters.length;
+    };
+
+    constructor(){
+        this.id = nanoid.nanoid();
+        this.allScooters.push(this); //Do this last.
+    };
+
+    startCharging(){
+        setTimeout(this.charged = true, 10000);
+    };
+};
+
+module.exports = Scooter;
+
+const Scooter = require("./Scooter")
+
+describe("A scooter should", () => {
+    const scooter1 = new Scooter()
+    const scooter2 = new Scooter()
+    TextDecoderStream("not be charged after 9 seconds", () => {
+        scooter1.charged = false
+        scooter1.startCharging()
+        setTimeout(expect(scooter1.charged)toBe(false), 9000)
+    })
+    test("be charged after 13 seconds", () => {
+        scooter2.charged = false
+        scooter2.startCharging()
+        settimeout
+    })
+})
--- scooterHireSystem/script.js
+++ scooterHireSystem/script.js
@@ -1,53 +1,92 @@
 "use strict";
-const nanoid = require("nanoid")
+const nanoid = require("nanoid");
+
+const app = {
+    loggedInUsers: [],
+    
+}
 
 class User{
-    constructor(userID){
-        
-    }
-}
+    constructor(age){
+        this.userID=nanoid.nanoid();
+        this.age=age;
+    };
+    logIn(){
+        if (age>=18){
+            app.loggedInUsers.push(this);
+        }else{
+            throw new Error("You have failed the vibe check. :c");
+        }
+    };
+};
 
 
 
 class Customer extends User{
-    constructor(BankBalance){
+    constructor(bankBalance){
 
-        super()
-        this.BankBalance=BankBalance
-    }
+        //super(userID);
+        if(!bankBalance){// if statements must be declared within the constructor
+
+ 
+       } 
+       
+       this.bankBalance=bankBalance;
+    };
+
+    
+   
     rentScooter(){
 
-        
-    }
+        ChargingDock.availableScooters.pop([]);
+    };
 
     returnScooter(){
-
+        ChargingDock.chargingScooters.shift();
         
-    }
+    };
     
-    returnBrokenScooter(){
+    reportBrokenScooter(){
+        ChargingDock.brokenScooters.shift()
 
 
         
-    }
+    };
 
-}
+};
+
+const testCustomer = new Customer('0');
 
 class maintW extends User{
     
-}
+};
 
 class Scooter {
-    // static scooters =  
+    static scooters = [];
     maxRange = 32;
-
+    charged = true;
+    broken = false;
     constructor(scooter){
         this.scooter = scooter;
-    }
-}
+        this.scooterID = nanoid.nanoid();
+    };
+};
 
 
 class ChargingDock{
-
+    chargingScooters=[];
     
-}
+    availableScooters=[];
+    
+    brokenScooters=[];
+
+    chargeAScooter(){
+        
+        .push(this)
+        
+    };
+    
+};
+
+
+
