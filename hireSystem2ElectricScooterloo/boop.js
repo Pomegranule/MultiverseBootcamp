dc66400cdec9dd23cfc8396bc13d7d2e14120742
Pomegranule <elleg1@gmx.com>
2022-05-02 18:32:57 +0100

    Okay so I decided to start my solo arc
    And it went okay, I guess. I'm slowly but surely getting this. I have also added an HTML file for easier debugging. Because wth even is node, anyways?

---
+++ hireSystem2ElectricScooterloo/boop.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class User{
+   constructor(age){// "age" is defined when a new user is created using this constructor class.
+       this.age = age;
+       this.userID = Math.random().toString(36).slice(2);
+   };
+
+   userLogIn(){
+       const userInput = document.getElementById("userIDInput").value();
+
+       if (userInput === this.userID){
+           console.log(`Welcome back, ${customer.userName}!`);
+       }else if(userInput === staff.userID){
+           console.log(`Welcome back, ${staff.userName}!`);
+       }else {
+           console.log(`${userInput} is not a valid user ID.`);
+       };
+   };
+
+//    noobieUserLogIn(){
+//         console.log("Welcome to Hire System 2: The Electric Scooterloo!");
+//         let u = prompt("Please input your userID:").value;
+        
+//         if (idInput === )
+//    };
+
+//    ogUserLogIn(){
+
+//    };
+} 
+
+const eleanor = new User(24)
+eleanor.userLogIn();
